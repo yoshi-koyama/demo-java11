33f9136b1d653fd9b1731112302d6b07a1880f5f
小山由人 <koyama@reytech.co.jp>
2022-08-12 21:41:09 +0900

    set up project

---
+++ src/main/java/com/raisetech/demojava11/DemoJava11Application.java
@@ -0,0 +1,13 @@
+package com.raisetech.demojava11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJava11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJava11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisetech/demojava11/DemoJava11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech.demojava11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJava11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
